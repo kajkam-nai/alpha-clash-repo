619656592e21258978507807fa5eb68f9479de3c
kajkam nei <habluhablu1234@gmail.com>
2024-08-21 22:52:59 +0600

    gameover

--- script/alpha-clash.js
+++ script/alpha-clash.js
@@ -7,6 +7,14 @@ function continueGame(){
    
 }
 
+// function getElementValue(valueId){
+//     const currentValue = document.getElementById(valueId);
+//     const value = currentValue.innerText;
+//     const properValue = parseInt(value);
+
+// }
+
+
 function keyHandlerButton(event){
     const pressKeyAlphabet = event.key;
     const currentAlphabet = document.getElementById('current-alphabet');
@@ -16,12 +24,46 @@ function keyHandlerButton(event){
     if(pressKeyAlphabet === expectedAlphabet){
         backgroundColorRemove(expectedAlphabet);
         continueGame();
+        const currentScoreElement = document.getElementById('current-score');
+        const currentText = currentScoreElement.innerText;
+        const currentValue = parseInt(currentText);
+        const newScore = currentValue + 1;
+        currentScoreElement.innerText = newScore;
+        const newsScore = document.getElementById('score');
+        newsScore.innerText= newScore;
     }
-    else
-    console.log('you lost your life');
+    else{
+        const currentLifeElement = document.getElementById('current-life');
+        const currentLifeText = currentLifeElement.innerText;
+        const currentLifeValue = parseInt(currentLifeText);
+        const newLifeScore = currentLifeValue - 1;
+        currentLifeElement.innerText = newLifeScore;
 
+        // const currentScoreElement = document.getElementById('current-score');
+        // const currentText = currentScoreElement.innerText;
+        // const currentValue = parseInt(currentText);
+        // const newScore = currentValue - 2;
+        // currentScoreElement.innerText = newScore;
 
+        if(newLifeScore === 0){
+            console.log('play end');
+            getElementId('play-ground');
+            getShowId('again-play');
+            // const removeHidden = document.getElementById('again-play');
+            // removeHidden.classList.remove('hidden');
+        }
+            
+
+    }
+        
+        // console.log('you lost your life');     
 }
+
+function setElementValue(elementId, value){
+    const element = document.getElementById(elementId);
+    element.innerText = value;
+}
+
 document.addEventListener('keyup',keyHandlerButton);
 
 function play(){
@@ -29,7 +71,13 @@ function play(){
     homeScreen = getElementId('home-screen');
     playGround = getShowId('play-ground');
     continueGame();
-    
-    
+}
 
+
+function playAgain(){
+    getElementId('again-play');
+    getShowId('play-ground');
+    setElementValue('current-score', 0)
+    setElementValue('current-life', 5)
+    continueGame();
 }
